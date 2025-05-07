f67667d065a6fc6dfebaa04fa3a172af3303b7fd
Natalie8a <naty.8a@outlook.com>
2025-05-07 04:42:23 +0000

    Added fetch, still empty array

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+//user url https://playground.4geeks.com/todo/users/N8Atest
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	
+	const [toDoList, setToDoList] = useState([])
+	const [newToDo, setNewToDo] = useState("")
+	const [showX, setShowX] = useState(null)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const handlePressKey = (e) => {
+		if (e.key === "Enter") {
+			setToDoList([...toDoList, newToDo])
+			setNewToDo("")
+		}
+	}
+	const handleDelete = (indexToDelete)=> {
+		console.log("delete")
+		setToDoList(toDoList.filter((elem, index) => index !== indexToDelete))
+	}
+
+useEffect(() => {
+	//API GET request
+	fetch("https://playground.4geeks.com/todo/users/N8Atest"
+	)
+	.then((response) => response.json())
+	.then((data) => console.log(data)) // data --> obect {name, toDos:[]}
+}, [])
+
+return (
+	<div className="box">
+	<div className="text-center">
+		<div id="title">ToDoList</div>
+		<div id="container-toDo-list">
+			<ul>
+				<li>
+					<input
+					className="text-box"
+					type="text"
+					placeholder="What needs to be done?"
+					value={newToDo}
+					onChange={(e) => setNewToDo(e.target.value)}
+						onKeyDown={handlePressKey} 
+					/>
+				</li>
+				{
+					toDoList.map((toDo, index) => (
+						<li
+						className="d-flex justify-content-between"
+							onMouseOver = { () => setShowX(index)}
+
+							>{toDo}
+							{showX === index && <small className="delete" onClick={() => handleDelete(index)}>x</small>}
+							
+						</li>
+					))
+				}
+				<li id="counter">{toDoList.length === 0 ? "No tasks left to do" : toDoList.length + "  Tasks left!"}</li>
+			</ul>
 		</div>
-	);
+	</div>
+	</div>
+)
+
 };
 
 export default Home;
