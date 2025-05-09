1fa183dc74def235d2ca1a25e7ea8ce3f3c66ea2
Natalie8a <naty.8a@outlook.com>
2025-05-09 22:47:58 +0000

    it finally worksgit add .git add .

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,29 +6,76 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //user url https://playground.4geeks.com/todo/users/N8Atest
 //create your first component
 const Home = () => {
-	
-	const [toDoList, setToDoList] = useState([])
-	const [newToDo, setNewToDo] = useState("")
-	const [showX, setShowX] = useState(null)
+	const [toDoList,setToDoList] = useState(["ABC","DEF"]);
+	const [newToDo,setNewToDo] = useState("");
+	const [flagDelete,setFlagDelete] = useState(null);
+	const handlePressKey = (e)=> {
+		if (e.key === "Enter"){
+			fetch("https://playground.4geeks.com/todo/todos/N8Atest",{
+				method:"POST",
+				body: JSON.stringify({
+					"label":newToDo,
+						"is_done":false
+					}),
+				headers:{
+					"Content-Type":"application/json"
+				}
+			})
+			.then((response)=> response.json())
+			//.then((data)=> console.log(data))
 
-	const handlePressKey = (e) => {
-		if (e.key === "Enter") {
-			setToDoList([...toDoList, newToDo])
-			setNewToDo("")
+			setNewToDo("");
+			fetch("https://playground.4geeks.com/todo/users/N8Atest")
+			.then((response)=> response.json())
+			.then((data)=> setToDoList(data.todos))
+			.then((data)=>console.log(data.todos))
+			//setTodoList([...todoList,newTodo]);
 		}
+		console.log([...toDoList,newToDo]);
 	}
-	const handleDelete = (indexToDelete)=> {
-		console.log("delete")
-		setToDoList(toDoList.filter((elem, index) => index !== indexToDelete))
+	const handleDelete = (taskId)=> {
+		//setTodoList(todoList.filter((item,idx)=> idx !== taskId ));
+		fetch("https://playground.4geeks.com/todo/todos/"+taskId,{
+			method : "DELETE"
+		})
+		.then((response) => {
+			if(response.ok){
+				console.log("Deleted task")
+				fetch("https://playground.4geeks.com/todo/users/N8Atest")
+				.then((response)=> response.json())
+				.then((data)=> setToDoList(data.todos))
+			}
+		})
+		//console.log("Eliminar elemento");
 	}
+	//console.log(newTodo);
 
-useEffect(() => {
-	//API GET request 
-	fetch("https://playground.4geeks.com/todo/users/N8Atest" //POST
-	)
-	.then((response) => response.json())
-	.then((data) => console.log(data)) // data --> obect {name, toDos:[]}
-}, [])
+const panic= async ()=>{
+		fetch("https://playground.4geeks.com/todo/users/N8Atest",{
+			method:"DELETE"
+		})
+		.then( async (response)=>{
+			if(response.ok){
+				console.log("Eliminacion correcta")
+				await fetch("https://playground.4geeks.com/todo/users/N8Atest",{
+					method:"POST",
+					headers:{
+						"Content-Type":"application/json"
+					}
+				})
+			}
+		}).then(async ()=>{
+			 await fetch("https://playground.4geeks.com/todo/users/N8Atest")
+				.then((response)=> response.json())
+				.then((data)=> setToDoList(data.todos)
+				)
+		})
+	}
+	useEffect(()=>{
+		fetch("https://playground.4geeks.com/todo/users/N8Atest")
+		.then((response)=> response.json())
+		.then((data)=> setToDoList(data.todos))
+	},[])
 
 return (
 	<div className="box">
@@ -42,18 +89,17 @@ return (
 					type="text"
 					placeholder="What needs to be done?"
 					value={newToDo}
-					onChange={(e) => setNewToDo(e.target.value)}
-						onKeyDown={handlePressKey} 
+					onChange={((e) => setNewToDo(e.target.value))}
+						onKeyDown={(e)=>(handlePressKey(e))} 
 					/>
 				</li>
 				{
-					toDoList.map((toDo, index) => (
-						<li
-						className="d-flex justify-content-between"
-							onMouseOver = { () => setShowX(index)}
-
-							>{toDo}
-							{showX === index && <small className="delete" onClick={() => handleDelete(index)}>x</small>}
+					toDoList.map((item) => (
+						<li key={item.id} className="d-flex justify-content-between py-2 px-5 w-100 border-bottom border-1 position-relative bg-light"
+							onMouseOver = { () => (setFlagDelete(item.id))}
+							onMouseLeave={()=> (setFlagDelete(null))}>
+							<span className="py-2">{item.label}</span>
+							{flagDelete === item.id && <small className="mx-3 text-end position-absolute top-50 end-0 translate-middle-y" onClick={()=>(handleDelete(item.id))}>x</small>} 
 							
 						</li>
 					))
@@ -62,6 +108,7 @@ return (
 			</ul>
 		</div>
 	</div>
+	<button className="button button-danger" onClick={()=>panic()}>PANIC</button>
 	</div>
 )
 
